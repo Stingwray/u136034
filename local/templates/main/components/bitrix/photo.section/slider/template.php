09c4f869bc395a6cfb7ea989db76338460ef90a8
Koshkoval K <mrqaz95@mail.ru>
2015-11-20 22:11:35 +0300

    Slider has been done

---
+++ local/templates/main/components/bitrix/photo.section/slider/template.php
@@ -0,0 +1,10 @@
+<!--nivo slider-->
+<div class="theme-default">
+	<div class="ribbon"></div>
+	<div id="slider3" class="nivoSlider">
+				<?foreach($arResult["ROWS"][0] as $key => $arItem):?>
+					<a href="<?=$arItem["LINK"];?>"><img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"];?>" width="970" height="360" alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"];?>"></a>
+				<?endforeach;?>
+	</div>
+</div>
+<!--/nivo slider-->
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -97,22 +97,20 @@
                 <li><a href="#"><img src="<?=SITE_TEMPLATE_PATH;?>/images/social_icons/v.png" width="30" height="27" alt="" /></a></li>
             </ul>
             <!-- /social tabs-->
-            <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"top_menu", 
-	array(
-		"COMPONENT_TEMPLATE" => "top_menu",
-		"ROOT_MENU_TYPE" => "top",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"MENU_CACHE_GET_VARS" => array(
+            <?$APPLICATION->IncludeComponent("bitrix:menu", "top_menu", Array(
+	"COMPONENT_TEMPLATE" => ".default",
+		"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
+			0 => "",
 		),
-		"MAX_LEVEL" => "2",
-		"CHILD_MENU_TYPE" => "left",
-		"USE_EXT" => "Y",
-		"DELAY" => "N",
-		"ALLOW_MULTI_SELECT" => "N"
+		"MAX_LEVEL" => "2",	// Уровень вложенности меню
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"USE_EXT" => "Y",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
 	),
 	false
 );?><br>
@@ -137,7 +135,62 @@
 
         <!--carousel-->
         <div id="carousel_container">
-            <!--nivo slider-->
+
+            <?$APPLICATION->IncludeComponent("bitrix:photo.section", "slider", Array(
+	"COMPONENT_TEMPLATE" => ".default",
+		"IBLOCK_TYPE" => "photos",	// Тип инфоблока
+		"IBLOCK_ID" => "15",	// Инфоблок
+		"SECTION_ID" => $_REQUEST["SECTION_ID"],	// ID раздела
+		"SECTION_CODE" => "",	// Код раздела
+		"SECTION_USER_FIELDS" => array(	// Свойства раздела
+			0 => "",
+			1 => "",
+		),
+		"ELEMENT_SORT_FIELD" => "sort",	// По какому полю сортируем фотографии
+		"ELEMENT_SORT_ORDER" => "asc",	// Порядок сортировки фотографий в разделе
+		"FILTER_NAME" => "arrFilter",	// Имя массива со значениями фильтра для фильтрации элементов
+		"FIELD_CODE" => array(	// Поля
+			0 => "ID",
+			1 => "Название",
+			2 => "Сортировка",
+			3 => "Картинка для анонса",
+		),
+		"PROPERTY_CODE" => array(	// Свойства
+			0 => "URL",
+			1 => "",
+		),
+		"PAGE_ELEMENT_COUNT" => "20",	// Количество элементов на странице
+		"LINE_ELEMENT_COUNT" => "4",	// Количество фотографий, выводимых в одной строке таблицы
+		"SECTION_URL" => "",	// URL, ведущий на страницу с содержимым раздела
+		"DETAIL_URL" => "",	// URL, ведущий на страницу с содержимым элемента раздела
+		"AJAX_MODE" => "N",	// Включить режим AJAX
+		"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+		"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+		"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+		"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+		"CACHE_TYPE" => "A",	// Тип кеширования
+		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+		"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+		"META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
+		"META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
+		"BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
+		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+		"SET_STATUS_404" => "N",	// Устанавливать статус 404, если не найдены элемент или раздел
+		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+		"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+		"PAGER_TITLE" => "Фотографии",	// Название категорий
+		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+	),
+	false
+);?>
+           <!--nivo slider
             <div class="theme-default">
                 <div class="ribbon"></div>
                 <div id="slider3" class="nivoSlider">
@@ -147,7 +200,7 @@
                     <a href="<?=SITE_TEMPLATE_PATH;?>/blog.html"><img src="<?=SITE_TEMPLATE_PATH;?>/images/temp/slider3.jpg" width="970" height="360" alt="" /></a>
                 </div>
             </div>
-            <!--/nivo slider-->
+            -->
         </div>
         <div id="slider3-caption-0" class="nivo-html-caption"><?$APPLICATION->IncludeComponent(
                 "bitrix:main.include",
